d326682cb257a34003fe748013948342c8b722dd
hostping <mailta@ya.ru>
2024-07-26 18:28:56 +0300

    fix1

--- backup_db.py
+++ backup_db.py
@@ -2,7 +2,26 @@
 
 import urllib.request
 import datetime
+import smtplib
 
 weekday = str(datetime.datetime.today().weekday());
-filename = "./dump/epr"+weekday+".zip";
-urllib.request.urlretrieve("https://www.electronpribor.ru/epr.zip", filename);
+srs_filename = "https://www.electronpribor.ru/epr.zip";
+dst_filename = "./dump/epr"+weekday+".zip";
+urllib.request.urlretrieve(srs_filename, dst_filename);
+
+#send post messages
+#smtp_server = "smtp.gmail.com"
+#port = 587
+#server = smtplib.SMTP(smtp_server, port)
+#server.starttls()
+
+#email = "your_email@gmail.com"
+#password = "your_password"
+#server.login(email, password)
+#from_email = email
+#to_email = "recipient_email@example.com"
+#subject = "Тестовое сообщение"
+#message = "Привет, это тестовое сообщение, отправленное с помощью Python и SMTP."
+#server.sendmail(from_email, to_email, f"Subject: {subject}\n\n{message}")
+
+#server.quit()
